095329dea83425b278cda37428626dc55aca8372
Ancel Puthoor <ancelputhoor88@gmail.com>
2025-02-16 16:11:29 +0530

    successfull in copying rotation

--- src/getdata.ts
+++ src/getdata.ts
@@ -1,40 +1,66 @@
+import { element } from "three/tsl";
 import { change } from "./main";
 
-var isAnimating=true
- function callapi() {
-    fetch('http://127.0.0.1:5000/')
-  .then(response => response.json())
-  .then(data => plotpoint(data))
-  .catch(error => console.error('Error:', error));
+var isAnimating = true;
+function callapi() {
+  fetch("http://127.0.0.1:5000/")
+    .then((response) => response.json())
+    .then((data) => plotpoint(data))
+    .catch((error) => console.error("Error:", error));
 }
 
-function plotpoint(values:any){
-    var landmarks=Object.keys(values).map((key)=>{
-        return values[key]
-    })
-    console.log(landmarks)
-    //  updatePointsfromapi(landmarks)
-    change(landmarks)
-   
-    if (isAnimating) {
-       
-        requestAnimationFrame(callapi)
-    }
-    
+function plotpoint(values: any) {
+  var landmarks = Object.keys(values).map((key) => {
+    return values[key];
+  });
+  // console.log(landmarks)
+  //  updatePointsfromapi(landmarks)
+  change(landmarks);
+
+  if (isAnimating) {
+    requestAnimationFrame(callapi);
+  }
 }
 
-export function starter(){
-    if (isAnimating) {
-       
-        requestAnimationFrame(callapi)
-    }
+export function starter() {
+  if (isAnimating) {
+    requestAnimationFrame(callapi);
+  }
 }
 
+const btn = document.getElementById("call");
+btn?.addEventListener("click", function () {
+  isAnimating = false;
+});
+const btn1 = document.getElementById("start");
+btn1?.addEventListener("click", starter);
 
-const btn=document.getElementById("call")
-btn?.addEventListener("click",function () {
-    isAnimating=false
-   
-})
-const btn1=document.getElementById("start")
-btn1?.addEventListener("click",starter)
+async function randompints() {
+  var targets = [
+    [1, 0, 0],
+    [1, 0.2, 0],
+    [1, 0.4, 0],
+    [1, 0.6, 0],
+    [1, 0.8, 0],
+    [1, 0.81, 0],
+    [1, 0.82, 0],
+    [1, 0.83, 0],
+    [1, 0.84, 0],
+    [1, 0.84, 0],
+    [1, 0.85, 0],
+    [1, 0.86, 0],
+    [1, 1, 0],
+    [1, 1, 0],
+    [1, 1, 0],
+    [1, 1, 0],
+    [1, 1, 0],
+    [1, 1, 0],
+    [1, 1, 0],
+  ];
+  const sleep = (ms: number) => new Promise((r) => setTimeout(r, ms));
+  for (const element of targets) {
+    await sleep(2000);
+    console.log(element)
+    change(element);
+  }
+}
--- src/main.ts
+++ src/main.ts
@@ -2,20 +2,21 @@ import * as THREE from "three";
 
 import { GLTFLoader, OrbitControls } from "three/examples/jsm/Addons.js";
 import { poseBones } from "./bones";
-import { starter } from "./getdata";
-
-
+import { Rotationhistory } from "./rotationhistory";
+import { arrowHelper1, arrowHelper2, drawline } from "./vectorvisualize";
 
 var scene: THREE.Scene,
   camera: THREE.Camera,
   renderer: THREE.WebGLRenderer,
   controls: OrbitControls;
-var skeltonhelper: THREE.SkeletonHelper, skeleton:any, clock: THREE.Clock;
+var skeltonhelper: THREE.SkeletonHelper, skeleton: any, clock: THREE.Clock;
 
-export var model:any;
+export var model: any;
 
 const h = window.innerHeight;
 const w = window.innerWidth;
+
+const boneRotationHistory = new Rotationhistory();
 
 function init() {
   renderer = new THREE.WebGLRenderer({ antialias: true });
@@ -48,9 +49,10 @@ function init() {
   const dirlight = new THREE.DirectionalLight("rgb(255, 255, 255)", 1);
   scene.add(dirlight);
   const axesHelper = new THREE.AxesHelper(2);
-  scene.add(axesHelper)
+  scene.add(axesHelper);
 
-
+  scene.add(arrowHelper1);
+  scene.add(arrowHelper2);
 }
 
 function loadmodel() {
@@ -60,9 +62,7 @@ function loadmodel() {
     model = gltf.scene;
     scene.add(model);
 
-    model.traverse((object:any) => {
-      
-
+    model.traverse((object: any) => {
       if (object.type === "SkinnedMesh") {
         skeleton = object.skeleton;
         console.log("Found skeleton:", skeleton);
@@ -72,42 +72,133 @@ function loadmodel() {
       //  console.log(skeleton)
       // }
     });
+
     skeltonhelper = new THREE.SkeletonHelper(gltf.scene);
     scene.add(skeltonhelper);
   });
 }
 
-export function change(target:any[]) {
- 
-  const axesHelper = new THREE.AxesHelper(1);
+export function change(target: any[]) {
+  //   const axesHelper = new THREE.AxesHelper(1);
 
- 
   const leftUpperArm = model.getObjectByName(poseBones.leftUpperArm);
+  const leftLowerArm = model.getObjectByName(poseBones.leftLowerarm);
 
-  leftUpperArm.add(axesHelper);
+  //   leftUpperArm.add(axesHelper);
+  //   leftLowerArm.add(axesHelper)
 
+  
+  //   let localDirection = new THREE.Vector3();
 
-  let localDirection1 = new THREE.Vector3(0, 1, 0);
-  let targetDirection = new THREE.Vector3(target[0][0], target[0][1], target[0][2]);
-  let current=localDirection1.clone().applyQuaternion(leftUpperArm.quaternion)
-  current.normalize();
+  let targetDirection = new THREE.Vector3(
+    target[0][0],
+    target[0][1],
+    target[0][2]
+  );
 
-  console.log("before",current)
- 
-  var rotationq=new THREE.Quaternion()
+  //   targetDirection.normalize();
+  //   targetDirection.multiplyScalar(-1)
+
  
   
+  // let current3=localDirection.subVectors(current2,current1)
+  //   let current = getBoneVector(leftUpperArm);
+  //
 
+  let localDirection2 = new THREE.Vector3();
+  let localDirection3 = new THREE.Vector3();
+  let current = new THREE.Vector3();
+  leftUpperArm.getWorldPosition(localDirection2);
+  leftLowerArm.getWorldPosition(localDirection3);
+  current.subVectors(localDirection3, localDirection2);
+ 
+  current.normalize();
 
-  rotationq.setFromUnitVectors(current,targetDirection)
-  leftUpperArm.quaternion.premultiply(rotationq)
-  leftUpperArm.quaternion.normalize()
+  drawline([current.x, current.y, current.z], arrowHelper1, "ddiff");
+  drawline(
+    [targetDirection.x, targetDirection.y, targetDirection.z],
+    arrowHelper2,
+    "original"
+  );
+  // console.log("before", localDirection2);
 
-  console.log("after",localDirection1.clone().applyQuaternion(leftUpperArm.quaternion).normalize())
+  //
 
+  let rotationq = new THREE.Quaternion();
+  //   const angleToRotate = 0;
+
+  var rotationAxis,
+    angleToRotate = 0;
+
+  //   rotationq.setFromUnitVectors(current, targetDirection);
+
+  // console.log(rotationq)
+  var prev = boneRotationHistory.getLastRotation(leftUpperArm);
+  var axis;
+  if (prev == null) {
+    axis = targetDirection;
+    // angle=angleToRotate
+
+    // boneRotationHistory.addRotation(leftUpperArm, current);
+
+    boneRotationHistory.addRotation(leftUpperArm, targetDirection);
+
+    rotationAxis = new THREE.Vector3()
+      .crossVectors(current, targetDirection)
+      .normalize();
+    angleToRotate = current.angleTo(targetDirection);
+    // console.log(angleToRotate,rotationAxis)
+
+    //   console.log(rotationAxis,angleToRotate);
+    
+  } else {
+    axis = prev.axis;
+    //    angle=prev.angle
+    current=prev.axis
+    
+    rotationAxis = new THREE.Vector3()
+      .crossVectors(current, targetDirection)
+      .normalize();
+    angleToRotate = current.angleTo(targetDirection);
+
+  }
+  rotationq.setFromAxisAngle(rotationAxis, angleToRotate);
+  rotationq.normalize();
+ 
+
+  //   //     // rotationq.conjugate()
+ 
+    if (prev == null || !areVectorsEqual(axis, targetDirection)) {
+      // console.log(axis,targetDirection)
+      // quat.multiply(rotationq)
+      //  console.log("inside")
+      console.log( angleToRotate);
+      leftUpperArm.quaternion.premultiply(rotationq);
+      leftUpperArm.quaternion.normalize();
+      leftUpperArm.updateMatrix();
+      leftLowerArm.updateMatrix();
+      leftLowerArm.updateMatrixWorld(true);
+
+      leftUpperArm.updateMatrixWorld(true);
+      boneRotationHistory.addRotation(leftUpperArm, targetDirection);
+    }
+  
+
+  //   console.log("after",localDirection1.clone().applyQuaternion(leftUpperArm.quaternion).normalize())
 }
 
 
+function areVectorsEqual(
+  vec1: THREE.Vector3,
+  vec2: THREE.Vector3,
+  epsilon = 0.001
+) {
+  return (
+    Math.abs(vec1.x - vec2.x) < epsilon &&
+    Math.abs(vec1.y - vec2.y) < epsilon &&
+    Math.abs(vec1.z - vec2.z) < epsilon
+  );
+}
 
 function animate() {
   requestAnimationFrame(animate);
@@ -115,7 +206,6 @@ function animate() {
   if (model) {
     model.updateMatrixWorld(true);
     skeleton.update(clock.getDelta());
-   
   }
   controls.update();
 }
